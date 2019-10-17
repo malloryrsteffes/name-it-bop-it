a4ee11d05032cafa64a1aaac1f70583f52f3ea2c
lethalSid <senien.hicks@gmail.com>
2019-10-16 22:00:42 -0400

    added gender randomizer functions

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
     console.log("test route hit");
 
     db.Baby.findAll({
-      limit: 100
+      limit: 2000
       // where: {
       //   name: req.params.name
       // }
@@ -29,6 +29,77 @@ module.exports = function(app) {
       }
 
       firstLetter();
+    });
+  });
+
+  app.get("/api/randomizes/:gender", function(req, res) {
+    console.log("randomizes route hit");
+
+    db.Baby.findAll({
+      limit: 2000
+      // where: {
+      //   name: req.params.name
+      // }
+    }).then(function(Baby) {
+      res.json(Baby);
+
+      var gender = req.params.gender;
+
+      //Function sets a conditional when user chooses between a boy and a girl
+      function genderName() {
+        if(gender === "boy") {
+          boyRandomize();
+        } else if(gender === "girl") {
+          girlRandomize();
+        } else {
+          randomizes();
+        }
+      }
+
+      function boyRandomize() {
+        var babyBoyNames = [];
+
+        for (i = 0; i < Baby.length; i++) {
+
+          if(Baby[i].gender === "MALE") {
+            babyBoyNames.push(Baby[i].name)
+          }
+        }
+
+        var picker = Math.floor(Math.random() * babyBoyNames.length) + 1;
+
+        console.log(`\nLooking for Baby Boy names`);
+        console.log(`\nHow do you like ${babyBoyNames[picker]}?\n`);
+      }
+
+
+      function girlRandomize(){
+        var babyGirlNames = [];
+
+        for (i = 0; i < Baby.length; i++) {
+          //console.log(Baby[i].gender, Baby[i].name);
+
+          if(Baby[i].gender === "FEMALE") {
+            babyGirlNames.push(Baby[i].name)
+          }
+        }
+
+        var picker = Math.floor(Math.random() * babyGirlNames.length) + 1;
+
+        console.log(`\nLooking for Baby Girl names`);
+        console.log(`\nHow do you like ${babyGirlNames[picker]}?\n`);
+      }
+
+      //function targets a specific name with the letter beginning with a
+      function randomizes() {
+
+        var picker = Math.floor(Math.random() * Baby.length) + 1;
+        console.log(`\nLooking for Baby name`);
+        console.log(`\nHow do you like ${Baby[picker].name}?\n`);
+      }
+
+      //randomizes();
+      genderName();
     });
   });
 
