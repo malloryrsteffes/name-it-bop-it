b8831fdb71fa22e662e0cd0c7ced5ce41fdbf4bc
lethalSid <senien.hicks@gmail.com>
2019-10-17 14:37:09 -0400

    adjusted api routes for cars and boats

--- models/pet.js
+++ models/pet.js
@@ -1,10 +1,6 @@
 module.exports = function(sequelize, DataTypes) {
   var Pet = sequelize.define("Pet", {
-    name: DataTypes.STRING,
-    gender: DataTypes.STRING,
-    breed: DataTypes.STRING,
-    color: DataTypes.STRING,
-    species: DataTypes.STRING
+    name: DataTypes.STRING
   });
   return Pet;
 };
