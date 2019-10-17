a48189d88ecf04cb76dbe57affde4b1ef8dec16a
Aaron Jackson <aarontjackson@outlook.com>
2019-10-17 13:30:08 -0400

    New dataset for pet table

--- models/pet.js
+++ models/pet.js
@@ -1,6 +1,10 @@
 module.exports = function(sequelize, DataTypes) {
   var Pet = sequelize.define("Pet", {
-    name: DataTypes.STRING
+    name: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    breed: DataTypes.STRING,
+    color: DataTypes.STRING,
+    species: DataTypes.STRING
   });
   return Pet;
 };
